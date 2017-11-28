b13f03d1627c67d662735ca78aca41093cd32b29
Yennifer Pernia <perniayennifer@gmail.com>
2017-11-28 16:37:27 -0300

    Agragando mapa y ubicacion

--- src/providers/ubicacion/ubicacion.ts
+++ src/providers/ubicacion/ubicacion.ts
@@ -13,11 +13,14 @@ export class UbicacionProvider {
   constructor(private geolocation: Geolocation) {
     console.log('Hello UbicacionProvider Provider');
   }
+  ubicacion:any ={}
 
   iniciar_ubicacion(){
     let watch = this.geolocation.watchPosition();
     watch.subscribe((data) => {
       console.log(data);
+      this.ubicacion.lat = data.coords.latitude;
+      this.ubicacion.lbg = data.coords.longitude;
       // data can be a set of coordinates, or an error (if an error occurred).
       // data.coords.latitude
       // data.coords.longitude
