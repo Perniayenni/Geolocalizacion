3c01c26a3824e49cd1fd7fa83d8c1b764d849785
yenni <7095>
2017-12-01 07:29:52 -0300

    se llama el metodo en el app_componetn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import  {EstacionamientosProvider} from '../providers/estacionamientos/estacionamientos';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,8 +11,11 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public servEs:EstacionamientosProvider) {
     platform.ready().then(() => {
+
+      this.servEs.obtenerEstacionamientos();
+      console.log(this.servEs.estacionamientos);
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
