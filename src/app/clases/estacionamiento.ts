2f7761ab202bf0e312c0d0f55bae4b49b67632fa
Yennifer Pernia <perniayennifer@gmail.com>
2017-12-13 11:29:23 -0300

    Se agregan nuevos marcadores

--- src/app/clases/estacionamiento.ts
+++ src/app/clases/estacionamiento.ts
@@ -12,10 +12,11 @@ export class Estacionamiento {
   disponibles?: number;
   duracion?:string;
   valorDurac?:number;
+  empresa:any;
   horarios: Horarios[];
 
   constructor(id_es:number, nombreEs: string, direccion: string, lt: any, lng: any, capacidad: number, tipoEs: string,
-              alturaMaxima: number, disponibles: number) {
+              alturaMaxima: number, disponibles: number, empresa:any) {
     this.id_es = id_es;
     this.nombreEs = nombreEs;
     this.direccion = direccion;
@@ -25,5 +26,6 @@ export class Estacionamiento {
     this.tipoEs = tipoEs;
     this.alturaMaxima = alturaMaxima;
     this.disponibles = disponibles;
+    this.empresa = empresa;
   }
 }
